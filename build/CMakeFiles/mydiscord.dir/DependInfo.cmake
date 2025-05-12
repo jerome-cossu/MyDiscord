62c1017bbc2d8b7e175ce928e918ac00c390a63a
jerome-cossu <jerome.cossu@laplateforme.io>
2025-05-12 12:17:54 +0200

    :rocket: feat:  project push to main

---
+++ build/CMakeFiles/mydiscord.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/jerom/OneDrive/Bureau/C-C++/MyDiscord/main.c" "CMakeFiles/mydiscord.dir/main.c.obj" "gcc" "CMakeFiles/mydiscord.dir/main.c.obj.d"
+  "C:/Users/jerom/OneDrive/Bureau/C-C++/MyDiscord/models/chat_page.c" "CMakeFiles/mydiscord.dir/models/chat_page.c.obj" "gcc" "CMakeFiles/mydiscord.dir/models/chat_page.c.obj.d"
+  "C:/Users/jerom/OneDrive/Bureau/C-C++/MyDiscord/models/home_page.c" "CMakeFiles/mydiscord.dir/models/home_page.c.obj" "gcc" "CMakeFiles/mydiscord.dir/models/home_page.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/mydiscord.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mydiscord.dir/main.c.obj"
+  "CMakeFiles/mydiscord.dir/main.c.obj.d"
+  "CMakeFiles/mydiscord.dir/models/chat_page.c.obj"
+  "CMakeFiles/mydiscord.dir/models/chat_page.c.obj.d"
+  "CMakeFiles/mydiscord.dir/models/home_page.c.obj"
+  "CMakeFiles/mydiscord.dir/models/home_page.c.obj.d"
+  "libmydiscord.dll.a"
+  "mydiscord.exe"
+  "mydiscord.exe.manifest"
+  "mydiscord.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mydiscord.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
